f29c4c7bfce9b68414c1b420b2053d1872c11450
Matt Pham <matt@nava.io>
2022-03-29 14:06:22 -0400

    test

--- pages/api/idea.js
+++ pages/api/idea.js
@@ -7,91 +7,40 @@ export default async function handler(req, res) {
   const payload = {
     token: "Uxv7HcwJhGCBO5Kz51iEMzVq",
     trigger_id: trigger_id,
-    view: {
-      type: "modal",
-      title: {
-        type: "plain_text",
-        text: "Submit a helpdesk ticket",
-      },
-      callback_id: "submit-ticket",
-      submit: {
-        type: "plain_text",
-        text: "Submit",
-      },
-      blocks: [
-        {
-          block_id: "title_block",
-          type: "input",
-          label: {
-            type: "plain_text",
-            text: "Title",
-          },
-          element: {
-            action_id: "title",
-            type: "plain_text_input",
-          },
-          hint: {
-            type: "plain_text",
-            text: "30 second summary of the problem",
-          },
-        },
-        {
-          block_id: "description_block",
-          type: "input",
-          label: {
-            type: "plain_text",
-            text: "Description",
-          },
-          element: {
-            action_id: "description",
-            type: "plain_text_input",
-            multiline: true,
-          },
-          optional: true,
-        },
-        {
-          block_id: "urgency_block",
-          type: "input",
-          label: {
-            type: "plain_text",
-            text: "Importance",
-          },
-          element: {
-            action_id: "urgency",
-            type: "static_select",
-            options: [
-              {
-                text: {
-                  type: "plain_text",
-                  text: "High",
-                },
-                value: "high",
-              },
-              {
-                text: {
-                  type: "plain_text",
-                  text: "Medium",
-                },
-                value: "medium",
-              },
-              {
-                text: {
-                  type: "plain_text",
-                  text: "Low",
-                },
-                value: "low",
-              },
-            ],
-          },
-          optional: true,
-        },
-      ],
+    type: "modal",
+    callback_id: "gratitude-modal",
+    title: {
+      type: "plain_text",
+      text: "Gratitude Box",
+      emoji: true,
     },
+    submit: {
+      type: "plain_text",
+      text: "Submit",
+      emoji: true,
+    },
+    close: {
+      type: "plain_text",
+      text: "Cancel",
+      emoji: true,
+    },
+    blocks: [
+      {
+        type: "input",
+        block_id: "my_block",
+        element: {
+          type: "plain_text_input",
+          action_id: "my_action",
+        },
+        label: {
+          type: "plain_text",
+          text: "Say something nice!",
+          emoji: true,
+        },
+      },
+    ],
   };
-  const result = await axios.post(
-    `https://slack.com/api/views.open`,
-    JSON.stringify(payload)
-  );
+  const result = await axios.post(`https://slack.com/api/views.open`, payload);
   console.log(result);
   res.status(200).json({ text: "Opening a form!" });
 }
