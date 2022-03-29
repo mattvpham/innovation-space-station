ae2b12c963c507fdadab85daa367165bf6760724
Matt Pham <matt@nava.io>
2022-03-29 13:18:24 -0400

    Make a form

--- pages/api/idea.js
+++ pages/api/idea.js
@@ -1,8 +1,55 @@
 export default function handler(req, res) {
   console.log(req);
-  res
-    .status(200)
-    .json({
-      text: "Thanks for your request, we'll process it and get back to you.",
-    });
+  res.status(200).json({
+    title: {
+      type: "plain_text",
+      text: "Modal Title",
+    },
+    submit: {
+      type: "plain_text",
+      text: "Submit",
+    },
+    blocks: [
+      {
+        type: "input",
+        element: {
+          type: "plain_text_input",
+          action_id: "sl_input",
+          placeholder: {
+            type: "plain_text",
+            text: "Placeholder text for single-line input",
+          },
+        },
+        label: {
+          type: "plain_text",
+          text: "Label",
+        },
+        hint: {
+          type: "plain_text",
+          text: "Hint text",
+        },
+      },
+      {
+        type: "input",
+        element: {
+          type: "plain_text_input",
+          action_id: "ml_input",
+          multiline: true,
+          placeholder: {
+            type: "plain_text",
+            text: "Placeholder text for multi-line input",
+          },
+        },
+        label: {
+          type: "plain_text",
+          text: "Label",
+        },
+        hint: {
+          type: "plain_text",
+          text: "Hint text",
+        },
+      },
+    ],
+    type: "modal",
+  });
 }
